ee4445412848e7d4ffff8dff70934ebdae4c8b28
Stefano Pinti <stefanopinti93@gmail.com>
2023-01-15 03:32:47 -0500

    Setup the basic endpoints for both controllers that return empty DTOs with proper response codes for successful operations.

--- src/main/java/com/egt/challenge/controller/AddressController.java
+++ src/main/java/com/egt/challenge/controller/AddressController.java
@@ -1,11 +1,16 @@
 package com.egt.challenge.controller;
 
+import com.egt.challenge.dto.AddressDto;
 import com.egt.challenge.service.AddressService;
 import com.egt.challenge.service.PersonService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(AddressController.BASE_URL)
@@ -18,5 +23,33 @@ public class AddressController {
     @NonNull
     private final PersonService personService;
 
-    // TODO create the appropriate endpoints as outlined in the README
+    @GetMapping("/")
+    public ResponseEntity<List<AddressDto>> getAll() {
+        ArrayList addressList = new ArrayList<>();
+        addressList.add(new AddressDto());
+        addressList.add(new AddressDto());
+        addressList.add(new AddressDto());
+
+        return ResponseEntity.ok().body(addressList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AddressDto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(new AddressDto());
+    }
+
+    @PostMapping
+    public ResponseEntity<AddressDto> create(@RequestBody AddressDto address) {
+        return ResponseEntity.created(URI.create("/api/addresses/id_goes_here")).body(new AddressDto());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AddressDto> updateByID(@RequestBody AddressDto address, @PathVariable Long id) {
+        return ResponseEntity.ok().body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<AddressDto> deleteById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(null);
+    }
 }
--- src/main/java/com/egt/challenge/controller/PersonController.java
+++ src/main/java/com/egt/challenge/controller/PersonController.java
@@ -1,11 +1,16 @@
 package com.egt.challenge.controller;
 
+import com.egt.challenge.dto.PersonDto;
 import com.egt.challenge.dto.PersonMapper;
 import com.egt.challenge.service.PersonService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(PersonController.BASE_URL)
@@ -18,5 +23,43 @@ public class PersonController {
     @NonNull
     private final PersonMapper personMapper;
 
-    // TODO create the appropriate endpoints as outlined in the README
+    @GetMapping("/")
+    public ResponseEntity<List<PersonDto>> getAll() {
+        ArrayList personList = new ArrayList<>();
+        personList.add(new PersonDto());
+        personList.add(new PersonDto());
+        personList.add(new PersonDto());
+
+        return ResponseEntity.ok().body(personList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PersonDto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(new PersonDto());
+    }
+
+    @PostMapping
+    public ResponseEntity<PersonDto> create(@RequestBody PersonDto person) {
+        return ResponseEntity.created(URI.create("/api/persons/id_goes_here")).body(new PersonDto());
+    }
+
+    @PostMapping("/lastName")
+    public ResponseEntity<List<PersonDto>> createAndReturnMatchingBySurname(@RequestBody PersonDto person) {
+        ArrayList personList = new ArrayList<>();
+        personList.add(new PersonDto());
+        personList.add(new PersonDto());
+        personList.add(new PersonDto());
+
+        return ResponseEntity.created(URI.create("/api/persons/id_goes_here")).body(personList);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PersonDto> updateByID(@RequestBody PersonDto person, @PathVariable Long id) {
+        return ResponseEntity.ok().body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PersonDto> deleteById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(null);
+    }
 }
